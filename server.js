445e1eeaf77f77090db32f37095b0586cd53a923
Saketh Ram <sakethram88>
2024-09-07 22:39:52 -0400

    moved to downloads folder

---
+++ server.js
@@ -0,0 +1,42 @@
+const express= require ("express");
+const app = express();
+const mongoose = require ("mongoose");
+const bodyParser= require ("body-parser");
+const path = require('path');
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+mongoose.connect("mongodb+srv://josyabhatlas1:saketh12388@tutoringregistration.uoed4.mongodb.net/")
+
+const dbschema ={
+    fullName : String,
+    course: String,
+    emailid: String,
+    TAname: String
+}
+
+const TutoringVisits = mongoose.model("TutoringVisits", dbschema);
+
+
+app.get("/", function(req,res) {
+    res.sendFile(__dirname+ "/index.html");
+
+})
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.post("/", function (req,res){
+    let newNote= new TutoringVisits({
+    fullName: req.body.fullName,
+    course: req.body.course,
+    emailid: req.body.emailid,
+    TAname: req.body.TAname
+
+});
+newNote.save();
+res.redirect("/");
+})
+
+app.listen(3000, function() {
+    console.log("server is running on 3000");
+})
